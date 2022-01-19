c4c4347b24b8adbbe43711f47908bebc0c5433e1
Bolotov Alexander <distalker1@gmail.com>
2022-01-19 14:42:52 +0300

    my 2 var

---
+++ src/main/java/web/config/handler/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,41 @@
+package web.config.handler;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+import web.config.exception.LoginException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    public CustomAuthenticationFailureHandler() {
+        super("/?error");
+    }
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+
+        // Запишем, чтобы не вводить заново ошибочные данные формы
+        if (isAllowSessionCreation()) {
+            LoginException loginException = new LoginException(exception.getMessage());
+            request.getParameterMap().forEach((key, value) -> {
+                if (key.equals("email")) {
+                    loginException.setEmail(value[0]);
+                } else if (key.equals("password")) {
+                    loginException.setPassword(value[0]);
+                }
+            });
+
+            request.getSession().setAttribute("Authentication-Exception", loginException);
+        }
+
+        super.onAuthenticationFailure(request, response, exception);
+    }
+}
